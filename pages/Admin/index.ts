e3cdeb550e4161e8476a28715bb76cdbb5b56728
Vagner de Oliveira <ti.vagner@gmail.com>
2023-02-08 23:29:04 -0300

    Create routes Admin

---
+++ pages/Admin/index.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+import withAuthAdmin from '../../components/withAuthAdmin';
+const Home: React.FC = () => {
+    return <h1>Parabéns </h1>;
+}
+
+export default withAuthAdmin(Home);
